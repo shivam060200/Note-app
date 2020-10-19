ce1803283400bea499ec36588e5f6bc4f9d83df5
shivam.pandya21minus1@gmail.com <shivam.pandya9@gmail.com>
2020-10-19 13:52:54 +0530

    Final Update

--- app.js
+++ app.js
@@ -1,54 +1,65 @@
-const chalk = require('chalk')
-const { describe } = require('yargs')
-const yargs = require('yargs')
- const getNote= require('./notes.js')
- //Customize yargs version
- yargs.version('1.1.0')
- //create add command
- yargs.command({
-     command: 'add',
-     describe: 'Add a new note',
-     //options for the command
-     builder:{
-           title: {
-                 describe: 'Note Title',
-                 demandOption: true, //title option should be provided
-                 type: 'string'   //by default its boolean
-           },
-           body:{
-                describe: 'Note body',
-                demandOption: true,
-                type: 'string'  
-           }
-     },
-     handler: function(argv){
-         console.log('Title: ' + argv.title)
-         console.log('Body: ' + argv.body)
-     }
-})
+const chalk = require("chalk");
+const { describe } = require("yargs");
+const yargs = require("yargs");
+const notes = require("./notes.js");
+//Customize yargs version
+yargs.version("1.1.0");
+//create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  //options for the command
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true, //title option should be provided while we write the command,by default its false
+      type: "string", //by default its boolean, so the type should be string
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    notes.addNote(argv.title, argv.body);
+  },
+});
 //Remove command
 yargs.command({
-    command: 'remove',
-    describe: 'removing note',
-    handler: function(){
-        console.log('Removing a note!')
-    }
-})
+  command: "remove",
+  describe: "removing note",
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    notes.remove(argv.title);
+  },
+});
 
 //list command
 yargs.command({
-    command: 'list',
-    describe: 'Listing all the notes',
-    handler: function(){
-        console.log('Listing the notes')
-    }
-})
-//reading a command
+  command: "list",
+  describe: "Listing all the notes",
+  handler: function () {
+    notes.list();
+  },
+});
+//reading  command
 yargs.command({
-    command: 'read',
-    describe: 'Reading a note',
-    handler: function(){
-        console.log('Reading the note')
-    }
-})
-yargs.parse()
+  command: "read",
+  describe: "Reading a note",
+  builder: {
+    title: "Note title",
+    demandOption: true,
+    type: "string",
+  },
+  handler: function (argv) {
+    notes.read(argv.title);
+  },
+});
+yargs.parse();
